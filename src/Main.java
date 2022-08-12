9424dfc45a3c85e5e5a0c38de46be174641fe5c7
OmidHdr <omidnet42@yahoo.com>
2022-08-12 23:11:56 +0430

    updateto version1.1 This program can now calculate the sum

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 
@@ -15,23 +17,39 @@ public class Main {
         if (prime == 0) {
 //            System.out.println("Not prime");
             return number;
-        }else{
+        } else {
             return 0;
         }
     }
 
     public static void main(String[] args) {
-        int number = 0, b = 0, m = 0;
+        int number = 0, b = 0, m = 0, multi = 0;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your number : ");
         ArrayList<Integer> list = new ArrayList<>();
         number = input.nextInt();
-
-        while(number > 1){
-            if ( prime(number)!= 0)
-               System.out.println(prime(number));
-//            System.out.println(number);
-            number -- ;
+        int Condition = number;
+        while (number > 1) {
+            if (prime(number) != 0) {
+                list.add(prime(number));
+//               System.out.println(prime(number));
+            }
+            number--;
+        }
+        Collections.sort(list);
+//        System.out.println(list);
+        int count = 1,i=0;
+        while(count<list.size()){
+            multi = list.get(i) + list.get(count);
+            if (multi > Condition) {
+                count++;
+                i = 1;
+            } else if (multi == Condition) {
+                System.out.print(list.get(i)+" + "+list.get(count) + " = "+Condition);
+                System.out.println();
+                i = 1;
+                count++;
+            }i ++;
         }
 
 
